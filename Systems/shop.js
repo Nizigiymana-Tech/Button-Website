3a924583def90a7bce05c1062db956e539d193ab
Terance Nizigiymana <Terance@webdev04.local>
2026-04-10 10:18:04 -0400

    Made shop button and shop itself

--- Systems/shop.js
+++ Systems/shop.js
@@ -1,1 +1,15 @@
-const button = document.getElementById("Shop")
+const button = document.getElementById("shopButton")
+const shopFrame = document.getElementById("shopFrame")
+
+button.addEventListener("click", onClicked)
+
+function onClicked() {
+    console.log(shopFrame.checkVisibility())
+    
+    if (shopFrame.style.visibility == "hidden") {
+        shopFrame.style.visibility = "visible"
+        return
+    } else {
+        shopFrame.style.visibility = "hidden"
+    }
+}
